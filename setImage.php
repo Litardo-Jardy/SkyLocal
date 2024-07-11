3adebb30e7927aee75a49d14f22b97a00d95369f
Litardo-Jardy <mirandalopez75445@gmail.com>
2024-07-10 23:56:15 -0500

    Implementacion de metodos para usar el algoritmo en forma de api

--- setImage.php
+++ setImage.php
@@ -4,52 +4,58 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); 
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_FILES['Image']) && isset($_POST['Folder']) && isset($_POST['id'])) {
-        $img = $_FILES['Image'];
-        $name_folder = $_POST['Folder'] + '/';
-        $id = $_POST['id'];
+function saveImage($image, $folder, $id){
+    $request['request'] = array();
+    $img = $_FILES['Image'];
+    $name_folder = $_POST['Folder'] + '/';
+    $id = $_POST['id'];
 
-        if (!is_dir($name_folder)) {
-            mkdir($name_folder, 0777, true);}
+    if (!is_dir($name_folder)) {
+        mkdir($name_folder, 0777, true);}
 
-        $targetFile = $name_folder . ($id + basename($_FILES["Image"]["name"]));
-        $uploadOk = true;
-        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $targetFile = $name_folder . ($id + basename($_FILES["Image"]["name"]));
+    $uploadOk = true;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $item = array();
 
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-        if ($check == false) {
-            echo "El archivo no es una imagen.<br>";
-            $uploadOk = false;
-            }elseif ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-                echo "Lo siento, solo se permiten archivos JPG, JPEG, PNG.<br>";
-                $uploadOk = false;
-            }elseif ($_FILES["image"]["size"] > 15000000){
-                echo "Lo siento, el archivo es demasiado grande.<br>";
-                $uploadOk = false;
-            }elseif (file_exists($targetFile)){
-                echo "Lo siento, el archivo ya existe.<br>";
-                $uploadOk = false;
-            }else {
-            echo "Procesado con exito.<br>";
-            $uploadOk = true;}
-
-
-        if (!$uploadOk) {
-            echo "Lo siento, tu archivo no fue subido.<br>";
-        } else {
-            if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-                echo "El archivo " . basename($_FILES["image"]["name"]) . " ha sido subido.<br>";
-                echo "La ruta completa del archivo es: " . realpath($targetFile) . "<br>";
-            } else {
-                echo "Lo siento, hubo un error al subir tu archivo.<br>";
-            }
-        }
-
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if ($check == false) {
+        array_push($item, "El archivo no es una imagen.", false);
+    }elseif ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+        array_push($item, "Solo se permiten archivos JPG. JPEG. PNG.", false);
+    }elseif ($_FILES["image"]["size"] > 15000000){
+        array_push($item, "EL archivo excede el peso permitido.", false);
+    }elseif (file_exists($targetFile)){
+        array_push($item, "EL archivo ya existe", false);
+    }else {
+            array_push($item, "EL archivo fue procesado con exito.", true);}
+        
+    if (!$item[1]) {
+        $items = array(
+            false,
+            $item[0]);
+        array_push($request['request'], $items);
+        return json_encode($request);
     } else {
-        echo "No se envió ningún archivo.<br>";
-    }
-} else {
-    echo "Método no permitido.<br>";
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            $items = array(
+                true,
+                realpath($targetFile));
+            array_push($request['request'], $items);}}
+    return json_encode($request);
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $request['request'] = array();
+    if (isset($_FILES['Image']) && isset($_POST['Folder']) && isset($_POST['id'])) {
+       $image = $_FILES['Image'];
+       $folder = $_POST['Folder'];
+       $id = $_POST['id'];
+
+       echo saveImage($image, $folder, $id);
+       
+    }else {
+    echo "No se envió ningún archivo.<br>";}
+}else {
+echo "Método no permitido.<br>";}
 ?>
